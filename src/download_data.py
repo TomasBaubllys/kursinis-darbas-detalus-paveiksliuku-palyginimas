622ded3dc7939f8793dd99f91cc166af75699ac2
Tomas <tomas.baublys@mif.stud.vu.lt>
2026-02-23 17:15:00 +0200

    added a python file to download the data

--- src/download_data.py
+++ src/download_data.py
@@ -9,14 +9,19 @@ import os
 
 file_id = '0B8-rUzbwVRk0c054eEozWG9COHM'
 url = f'https://drive.google.com/uc?id={file_id}'
-output = 'Market-1501.zp'
+output_zip = 'Market-1501.zip'
 relative_dir = '../data'
 
 def setup_market1501():
-    if not os.path.exists(os.path.join(os.getcwd(), relative_dir)):
-        data_dir = os.path.join(os.getcwd(), relative_dir)
-        print(f"Create a new directory: {data_dir}")
+    data_dir = os.path.join(os.getcwd(), relative_dir)
+    zip_file_full_path = os.path.join(data_dir, output_zip)
+    if not os.path.exists(data_dir):
+        print(f"Creating a new directory: {data_dir}")
         os.mkdir(data_dir)
+    if not os.path.exists(zip_file_full_path):
+        print(f"Downloading the dataset {output_zip} file")
+        gdown.download(url, zip_file_full_path, quiet=False)
+    
 
 setup_market1501()
 # gdown.download(url, output, quiet=False)
