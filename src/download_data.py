e73e544d257c4d326e93709876d55016d8cfad43
Tomas <tomas.baublys@mif.stud.vu.lt>
2026-02-23 20:35:50 +0200

    updated download file

--- src/download_data.py
+++ src/download_data.py
@@ -3,28 +3,37 @@
 # This file creates a folder if it doesnt exists in the root folder called 'data'
 # Downloads and unzips the Market1501 dataset from a know google-drive link
 
-import gdown
-import zipfile
 import os
+import zipfile
 
-file_id = '0B8-rUzbwVRk0c054eEozWG9COHM'
-url = f'https://drive.google.com/uc?id={file_id}'
-output_zip = 'Market-1501.zip'
-relative_dir = '../data'
+import gdown
+
+file_id = "0B8-rUzbwVRk0c054eEozWG9COHM"
+url = f"https://drive.google.com/uc?id={file_id}"
+output_zip = "Market-1501.zip"
+relative_dir = "../data"
+
 
 def setup_market1501():
-    data_dir = os.path.join(os.getcwd(), relative_dir)
-    zip_file_full_path = os.path.join(data_dir, output_zip)
-    if not os.path.exists(data_dir):
-        print(f"Creating a new directory: {data_dir}")
-        os.mkdir(data_dir)
-    if not os.path.exists(zip_file_full_path):
-        print(f"Downloading the dataset {output_zip} file")
-        gdown.download(url, zip_file_full_path, quiet=False)
-    print("Extracting the zip file")
-    with zipfile.ZipFile(zip_file_full_path, 'r') as zip_ref:
-        zip_ref.extractall(data_dir)
+    try:
+        data_dir = os.path.join(os.getcwd(), relative_dir)
+        zip_file_full_path = os.path.join(data_dir, output_zip)
+        if not os.path.exists(data_dir):
+            print(f"Creating a new directory: {data_dir}")
+            os.mkdir(data_dir)
+        if not os.path.exists(zip_file_full_path):
+            print(f"Downloading the dataset {output_zip} file")
+            gdown.download(url, zip_file_full_path, quiet=False)
+        print("Extracting the zip file")
+        with zipfile.ZipFile(zip_file_full_path, "r") as zip_ref:
+            zip_ref.extractall(data_dir)
+    except zipfile.BadZipFile as error:
+        print(error)
+    except PermissionError as error:
+        print(error)
+    except Exception as e:
+        print(f"Unknown error occurred {e}")
+
 
 if __name__ == "__main__":
     setup_market1501()
-
