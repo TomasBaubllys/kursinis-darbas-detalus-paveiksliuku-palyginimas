471762620f8ebc4c3fdea5ce09794ea0175c350a
Tomas Baublys <tomas.baublys@mif.stud.vu.lt>
2026-02-25 00:21:49 +0200

    added coments

--- src/contrastive_loss.py
+++ src/contrastive_loss.py
@@ -8,9 +8,11 @@ class Contrastive_Loss(nn.Module):
 
 	def forward(self, output1, output2, label):
 		euclidean_dist = nn.pairwise_distance(output1, output2)
-
+		
+		# minimize dist if the same person label = 1
 		pos = label * torch.pow(euclidean_dist, 2)
 
+		# maximize othewerwise (label = 0)
 		neg = (1 - label) * torch.pow(torch.clamp(self.margin - euclidean_dist, min = 0.0), 2)
 
 		return torch.mean(pos + neg)
