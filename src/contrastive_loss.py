b78a8f1ef3ce80baefcdbe7aa63d8c005b658185
Tomas Baublys <tomas.baublys@mif.stud.vu.lt>
2026-02-25 00:19:34 +0200

    implkemented closs

--- src/contrastive_loss.py
+++ src/contrastive_loss.py
@@ -2,6 +2,19 @@ import torch
 import torch.nn as nn
 
 class Contrastive_Loss(nn.Module):
-    def __init__(self):
+    def __init__(self, margin=2.0):
         super().__init__()
+		self.margin = margin
+
+	def forward(self, output1, output2, label):
+		euclidean_dist = nn.pairwise_distance(output1, output2)
+
+		pos = label * torch.pow(euclidean_dist, 2)
+
+		neg = (1 - label) * torch.pow(torch.clamp(self.margin - euclidean_dist, min = 0.0), 2)
+
+		return torch.mean(pos + neg)
+
+
+
 
